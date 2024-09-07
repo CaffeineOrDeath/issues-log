b01b464504da5fbbe921a018c078aefabfc9c35c
wjorden <jorden.williams.1989@gmail.com>
2024-09-07 18:36:59 -0400

    added second Handle, same error

--- templ/main.go
+++ templ/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-    "fmt"                       // debugging
-    "net/http"                  // basic server handling
+	"fmt"      // debugging
+	"net/http" // basic server handling
 
-    "github.com/a-h/templ"      // templ
+	"github.com/a-h/templ" // templ
 )
 
 func main() {
 
-    port := ":8080"
+	port := ":8080"
 
-    test := templ.Component(Test())
+	test := templ.Component(Test())
 
-    http.Handle("/", templ.Handler(test))
-    fmt.Println("Starting connection...")
-    http.ListenAndServe(port, nil)
+	http.Handle("/", templ.Handler(test))
+	// implemented, no dice
+	http.Handle("/styles/", http.StripPrefix("/styles", http.FileServer(http.Dir("./styles"))))
+	fmt.Println("Starting connection...")
+	http.ListenAndServe(port, nil)
 }
